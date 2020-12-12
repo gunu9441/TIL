883d1c71092b31842e3bc01a16de17edf6ac80dc
Gunu <gunu9441@gmail.com>
2020-12-12 18:27:34 +0900

    feature operating homepage

--- Web_basic/Nodejs/main.js
+++ Web_basic/Nodejs/main.js
@@ -7,10 +7,35 @@ var app = http.createServer(function(request,response){//address:http://localhos
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     console.log(url.parse(_url, true));
-    var title = queryData.id;
 
     if(pathname === '/'){
+      if(queryData.id === undefined){
+          var title = "Welcome";
+          var description = "Hello, Node.js";
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="http://localhost:3000/?id=CSS">CSS</a></li>
+              <li><a href="http://localhost:3000/?id=Javascript">Javascript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+         response.writeHead(200);
+         response.end(template);
+      } else {
       fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var title = queryData.id;
         var template = `
         <!doctype html>
         <html>
@@ -33,6 +58,7 @@ var app = http.createServer(function(request,response){//address:http://localhos
        response.writeHead(200);
        response.end(template);
        });
+      }
     } else {
       response.writeHead(404);
       response.end("Not found");
