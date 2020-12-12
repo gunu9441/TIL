8fdd287daf868ed91a898c6d5d18fd05ad1fd890
Gunu <gunu9441@gmail.com>
2020-12-12 12:58:50 +0900

    basic synstax of nodejs

---
+++ Web_basic/Nodejs/Syntax/string.js
@@ -0,0 +1,3 @@
+console.log(1+1);//산술연산
+console.log('1'+'1');//결합연산
+console.log('I have a lot of apples'.length);//하나의 갯수
---
+++ Web_basic/Nodejs/Syntax/template.js
@@ -0,0 +1,21 @@
+var name = 'gunu';
+var letter = 'Dear '+ name + '\nDolore minim consectetur '+ name +' veniam labore.\n\
+\Dolor est ex est Lorem adipisicing elit ut exercitation minim anim fugiat et.\n' + name +'Al\
+\ iquip sunt culpa duis ad veniam do laboris sit. Velit tempor duis excepteur incididunt sunt.\
+\ Laborum nulla qui quis dolor.\nCupidatat anim voluptate ipsum qui velit consectetur sint \
+\ voluptate culpa cillum non tempor minim ea.\n'
+console.log(letter)
+
+var letter = `Dear ${name} 
+
+
+
+Dolore minim consectetur ${name}  veniam labore.
+Dolor est ex est Lorem adipisicing elit ut exercitation minim anim fugiat et.
+${name} Aliquip sunt culpa duis ad veniam do laboris sit. Velit tempor duis excepteur incididunt sunt.
+Laborum nulla qui quis dolor.
+Cupidatat anim voluptate ipsum qui velit consectetur sint 
+voluptate culpa cillum non tempor ${1+3} minim ea. ${name}`;
+
+console.log(letter)
+
