447fd25b4e354c798841ee5cd7a95f30ba5db4e0
Gunu <gunu9441@gmail.com>
2020-12-14 14:04:49 +0900

    feature updating information

--- Web_basic/Nodejs/main.js
+++ Web_basic/Nodejs/main.js
@@ -3,18 +3,18 @@ var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
   <!doctype html>
    <html>
    <head>
-     <title>WEB1 - ${title}</title>
+     <title>WEB1 - ${title}</title> 
      <meta charset="utf-8">
    </head>
    <body>
      <h1><a href="/">WEB</a></h1>
      ${list}
-        <a href="/create">create</a>
+     ${control}
      ${body}
    </body>
    </html>`;
@@ -46,11 +46,13 @@ var app = http.createServer(function (request, response) {
         var title = "Welcome";
         var description = "Hello, Node.js";
         var list = templateList(filelist);
+        var control = '<a href="/create">create</a>'
 
         var template = templateHTML(
           title,
           list,
-          `<h2>${title}</h2><p>${description}</p>`
+          `<h2>${title}</h2><p>${description}</p>`,
+          control
         );
         response.writeHead(200);
         response.end(template);
@@ -60,10 +62,12 @@ var app = http.createServer(function (request, response) {
         fs.readdir("data/", function (err, filelist) {
           var title = queryData.id;
           var list = templateList(filelist);
+          var control = `<a href="/create">create</a> <a href ="/update?id=${title}">update</a>`
           var template = templateHTML(
             title,
             list,
-            `<h2>${title}</h2><p>${description}</p>`
+            `<h2>${title}</h2><p>${description}</p>`,
+            control
           );
           response.writeHead(200);
           response.end(template);
@@ -78,16 +82,16 @@ var app = http.createServer(function (request, response) {
         title,
         list,
         `
-      <form action="http://localhost:3000/create_process" method="POST">
+      <form action="/create_process" method="POST">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
-            <textarea name="description" placeholder="descripion"></textarea>
+            <textarea name="description" placeholder="description"></textarea>
         </p>
         <p>
             <input type="submit">
         </p>
         </form>
-      `
+      `, ` `
       );
 
       response.writeHead(200);
@@ -111,6 +115,45 @@ var app = http.createServer(function (request, response) {
         response.end("success"); // redirection
       });
     });
+  } else if (pathname == "/update"){
+    fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+      fs.readdir("data/", function (err, filelist) {
+        var title = queryData.id;
+        var list = templateList(filelist);
+        var control = `<a href="/create">create</a> <a href ="/update?id=${title}">update</a>`
+        var template = templateHTML(
+          title,
+          list,
+          `<form action="/update_process" method="POST">
+            <input type ="hidden" name ="id" value = "${title}">
+            <p><input type="text" name="title" placeholder="title" value ="${title}"></p>
+            <p>
+                <textarea name="description" placeholder="description">"${description}</textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+            </form>
+          `,
+          control
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  }else if(pathname==`/update_process`){
+    // var body =``
+    // request.on('data', function(data){
+    //   body += data;
+    // })
+    // request.on('end', function(){
+    //   var post = qs.parse(body);
+    //   var title = post.title;
+    //   var description = post.description;
+    //   console.log(title, description)
+    // }) 
+    response.writeHead(200);
+    response.end('success');
   } else {
     response.writeHead(404);
     response.end("Not found");
