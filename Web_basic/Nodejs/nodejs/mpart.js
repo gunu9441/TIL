2bcb66d03a836a44aa9a32315966087acc0acb0e
Gunu <gunu9441@gmail.com>
2020-12-15 11:39:35 +0900

    learn about module

---
+++ Web_basic/Nodejs/nodejs/mpart.js
@@ -0,0 +1,28 @@
+var M ={
+    v:'v',
+    f:function(){
+        console.log(this.v);
+    }
+};
+
+var O={
+    p:'p',
+    k:function(){
+        console.log(this.p)
+    }
+}
+
+var J = {
+    J:'J',
+    u:function(){
+        console.log(this.J)
+    }
+}
+
+// module.exports.M = M;
+// module.exports.O = O;
+console.log(module.exports)
+module.exports = {M,O}
+console.log(module.exports)
+// module.exports = {J}
+console.log(module.exports)
---
+++ Web_basic/Nodejs/nodejs/muse.js
@@ -0,0 +1,10 @@
+// var M ={
+//     v:'v',
+//     f:function(){
+//         console.log(this.v);
+//     }
+// }
+var mpart = require('./mpart.js')
+console.log(mpart)
+
+mpart.M.f();
