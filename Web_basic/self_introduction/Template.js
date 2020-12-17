120eba3ea83a2b9f87d106e4d992c751764c3851
Gunu <gunu9441@gmail.com>
2020-12-18 00:11:26 +0900

    make hteml & list template module

---
+++ Web_basic/self_introduction/Template.js
@@ -0,0 +1,34 @@
+module.exports = {
+    pageForm: function(list, name, description,control){
+        return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>self_introduction_${name}</title>
+            
+        </head>
+        <body>
+            <h1>Welcome to <a href="/">gunu's</a> page!</h1>
+            <h2>Let me know U!</h2>
+            ${list}
+            ${control}
+            <h3>${name}</h3>
+            ${description}
+            </div> 
+        </body>
+        </html>
+        `
+    },
+    pageList: function(filelist){
+        list =`<ol>`
+        var i = 0;
+        while(i < filelist.length){
+            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i += 1;
+        }
+        list += '</ol>';
+        return list;
+    }
+}
