0e449b06dd40db3f1b3f2fad1bb315125e95de46
Gunu <gunu9441@gmail.com>
2020-12-19 17:04:52 +0900

    practice handling component

--- Web_basic/Reactjs/movie_app/src/App.js
+++ Web_basic/Reactjs/movie_app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+function Food({fav}){
+  return <h1>I like {fav}</h1>;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+          <h1>Hello!!!!</h1>
+          <Food fav="kimchi"
+                papapa={true}
+                koko={["pangya",,1,2,3,true]} />
+        </div>;
 }
 
 export default App;
