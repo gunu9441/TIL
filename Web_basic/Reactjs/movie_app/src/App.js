3e8e4065727198c106004f07d95f92a80efcffbf
Gunu <gunu9441@gmail.com>
2020-12-21 12:17:42 +0900

    check life cycle of component

--- Web_basic/Reactjs/movie_app/src/App.js
+++ Web_basic/Reactjs/movie_app/src/App.js
@@ -11,7 +11,14 @@ class App extends React.Component{
   minus = () => {
     this.setState(current => ({ count: current.count - 1}));
   };
+  componentDidMount(){
+    console.log("component rendered")
+  }
+  componentDidUpdate(){
+    console.log("I just updated")
+  }
   render(){
+    console.log("I'm rendering")
     return <div>
       <h1>The number is: {this.state.count}</h1>
       <button onClick={this.add}>Add</button>
