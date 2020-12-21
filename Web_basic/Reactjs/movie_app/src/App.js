1c9f626d512c7f1ef155a5042347b8a1043b5ddf
Gunu <gunu9441@gmail.com>
2020-12-21 12:44:52 +0900

    build a base of movie app

--- Web_basic/Reactjs/movie_app/src/App.js
+++ Web_basic/Reactjs/movie_app/src/App.js
@@ -1,30 +1,21 @@
-import React from 'react';
-import PropTypes from "prop-types";
+import React from "react";
 
 class App extends React.Component{
-  state = {
-     count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1}));
-  };
-  componentDidMount(){
-    console.log("component rendered")
-  }
-  componentDidUpdate(){
-    console.log("I just updated")
-  }
-  render(){
-    console.log("I'm rendering")
-    return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-      </div>
-  }
+	state={
+		isLoading: true,
+		movies: []
+	}
+	componentDidMount(){
+		setTimeout(()=> {
+			this.setState({isLoading: false, book: true})
+		}, 6000)
+	}
+	render(){
+		const {isLoading} = this.state;
+		return <div>
+					 {isLoading ? "Loading...":"we are ready"} 
+					 </div>;
+	}
 }
 
 export default App;
