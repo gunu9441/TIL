7e175ee4558b01dd27521dffcc72bde071f63913
Gunu <gunu9441@gmail.com>
2020-12-23 14:58:09 +0900

    fix code with data from axios using se6

--- Web_basic/Reactjs/movie_app/src/App.js
+++ Web_basic/Reactjs/movie_app/src/App.js
@@ -2,24 +2,27 @@ import React from "react";
 import axios from "axios";
 
 class App extends React.Component{
-	state={
+	state = {
 		isLoading: true,
-		movies: []
-	}
-	getMovies = async()=>{
-		const movies = await axios.get("http://yts-proxy.now.sh/list_movies.json");
-		console.log(movies)
+		movies : []
+	};
+	
+	getMovies = async() => {
+		const {
+			data:{
+				data:{movies}
+			}
+		}= await axios.get("https://yts-proxy.now.sh/list_movies.json");
+		this.setState({movies:movies});
+		console.log(movies);
 	}
 	componentDidMount(){
 		this.getMovies();
 	}
 	render(){
 		const {isLoading} = this.state;
-		return <div>
-					 {isLoading ? "Loading...":"we are ready"} 
-					 </div>;
+		return <div>{isLoading ? "Loading..." : "We are ready"}</div>
 	}
 }
 
 export default App;
- 
