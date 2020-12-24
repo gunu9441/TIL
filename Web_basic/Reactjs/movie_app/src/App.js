205e632f5ad2406b4efb37d3b9ed4e3f839f5435
Gunu <gunu9441@gmail.com>
2020-12-24 12:57:33 +0900

    decorate view  by using css file

--- Web_basic/Reactjs/movie_app/src/App.js
+++ Web_basic/Reactjs/movie_app/src/App.js
@@ -23,22 +23,23 @@ class App extends React.Component{
 	render(){ 
 		const {isLoading, movies} = this.state;
 		console.log(movies)
-		return <section class="container">
+		return <section className="container">
 			{isLoading 
-				?  (<div class="loader">
-					<span class = "loader__text">Loading...</span>
+				?  (<div className="loader">
+					<span className = "loader__text">Loading...</span>
 				</div>) : 
 				(
-					<div class = "movies">
+					<div className = "movies">
 						{
 						movies.map(movie => (
 							<Movie 
-								key = {movie.id}
+								key={movie.id}
 								id={movie.id} 
 								year={movie.year} 
 								title={movie.title} 
 								summary={movie.summary} 
 								poster={movie.medium_cover_image} 
+								genres={movie.genres}
 								/>
 						))	
 						}
