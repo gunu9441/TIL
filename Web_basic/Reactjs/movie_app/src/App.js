4d379382b51706fee727b8107c2577787e9dcb30
Gunu <gunu9441@gmail.com>
2020-12-25 14:07:33 +0900

    structure frame of navigation function

--- Web_basic/Reactjs/movie_app/src/App.js
+++ Web_basic/Reactjs/movie_app/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {HashRouter, Route} from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Navigation from './components/Navigation';
 
 function App(){
-	return <span>something</span>;
+	return(
+		<HashRouter>
+			<Navigation/>
+			<Route path='/' component={Home}/>
+			<Route path='/about' component={About}/>
+		</HashRouter>	
+	);
 }
 
 export default App;
